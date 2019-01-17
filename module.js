0c25315b14339f17c4abbd1bd9513ba899e844a4
vicky <cutemuee@gmail.com>
2019-01-17 18:05:14 +0800

    1. openState 與 Autotoggle搭配性 2. autoToggle只會執行一次 3. 初始化 4. 移除option2 5. btn style 修正

--- module.js
+++ module.js
@@ -10,11 +10,10 @@
     this.option = opt;
     this.$img = $("img");
     this.$wrap = $(".wrap");
-    // this.state = $ele.data("state");
-    this.opened = opt.class.opened;
+    this.opened = opt.class.opened; 
     this.closed = opt.class.closed;
     this.toggleTime; //AutoToggle時判斷傳入true or number
-    this.timer ;
+
   };
 
   Module.DEFAULTS = {
@@ -32,79 +31,96 @@
       opening: "opening"
     },
     transition: true,
-    counter: 1500, //控制transition
+    counter: 1500, //控制transition 必須和CSS中秒數相同  
     whenTransition: function() {
       console.log("whenTransition");
     }
   };
   //開始寫Function
-  Module.prototype.init = function() {
-    this.$ele.append(this.$btn); // 1. button初始
-    var openAtStart = this.option.openAtStart; //2. 畫面是否先開
-    if (openAtStart === true) {
-      //可精簡
-      this.$btn.text(this.option.button.closeText).append(this.$up);
-      this.$ele.addClass(this.opened);
-      this.$wrap.addClass("opened");
-      this.$img.addClass("opened");
-    } else {
-      this.$btn.text(this.option.button.openText).append(this.$down);
-      this.$ele.addClass(this.closed);
-      this.$img.addClass("closed");
-      this.$wrap.addClass("closed");
-    }
+ 
 
-    if (this.option.transition) {
-      this.addTransition();
+  Module.prototype.init = function() {
+      
+    this.$ele.append(this.$btn); // 1. button初始
+     var openAtStart = this.option.openAtStart; //2. 畫面是否先開
+    if(this.option.autoToggle === false){  
+          //autoToggle :false --> 正常
+          console.log("toggle false");
+        if (openAtStart){
+            this.openedState();
+        }else{
+            this.closedState();
+        }
+    }else if(this.option.autoToggle === true){ //autoToggle: true --> 與Atstart相反
+        console.log("toggle true");
+        if (openAtStart){
+            this.closedState();
+        }else{
+            this.openedState();
+        }
+    }else{
+        if (openAtStart){
+            this.openedState();
+        }else{
+            this.closedState();
+        }
+        setTimeout(this.click.bind(this),this.option.autoToggle);  //autoToggle: number --> trigger click
     }
-    if (this.option.autoToggle) {
-      this.toggleTime = isNaN(this.option.autoToggle) ? this.option.autoToggle : 3000 ;
-      this.toggleTrigger();
+    if (this.option.transition) {    
+        this.addTransition();
+      }
+  
     }
-  };
-  Module.prototype.addTransition = function() {
+   
+  
+  Module.prototype.openedState = function(){
+    console.log("heyheyhey");
+    this.$btn.text(this.option.button.closeText).append(this.$up); 
+    this.$ele.addClass(this.opened);  
+    this.$wrap.addClass("opened");
+    this.$img.addClass("opened");
+  }
+  Module.prototype.closedState = function(){
+      console.log("catch");
+    this.$btn.text(this.option.button.openText).append(this.$down);
+    this.$ele.addClass(this.closed);
+    this.$img.addClass("closed");
+    this.$wrap.addClass("closed");
+  }
+  Module.prototype.addTransition = function() {    //可以多加!this.$ele.hasClass('transition)
     this.$wrap.addClass("transition");
   };
-  //原先將toggle綁定在click上,為了解決click之後能夠取消toggle, 因此另外架構
-  // Module.prototype.toggle = function(){
-  //     console.log("hastoggle");
-  //     if( isNaN(this.option.autoToggle) === false){
-  //         console.log("toggle=number");
-  //         this.timer_num = setInterval(this.click.bind(this),this.option.autoToggle);  //有數字設定toggle
-  //     }else if (this.option.autoToggle === true){
-  //         console.log("toggle = default");
-  //         this.timer_bln = setInterval(this.click.bind(this),this.option.counter); //預設值
-  //     }
-  // }
-  Module.prototype.toggle = function() {
-    if (this.$ele.hasClass(this.opened)) {
-      //
-      setTimeout(this.close(), this.toggleTime);
-    } else if (this.$ele.hasClass(this.closed)) {
-      setTimeout(this.open(), this.toggleTime);
-    }
-    this.Transition();
-  };
-  Module.prototype.toggleTrigger = function() {
-    this.timer = setInterval(this.toggle.bind(this), this.toggleTime);
-  };
+  
+//   Module.prototype.toggle = function(){
+//       this.toggleTime = ! isNaN(this.option.autoToggle) ? this.option.autoToggle : 0;
+//       console.log(this.option.autoToggle);
+      
+//       if( isNaN(this.option.autoToggle) === false){   //有數字設定toggle
+//           console.log("toggle=number");
+//           this.timer_num = setTimeout(this.click.bind(this),this.option.autoToggle);  
+//       }else {     //如果是true跑這一行      
+//           console.log("toggle = default");
+//           this.timer_bln = setTimeout(this.click.bind(this),0); // true > 瞬間開
+//       }
+//   }
+  
   Module.prototype.click = function() {
+     // clearTimeout(this.imgAction);
     if (this.$ele.hasClass(this.opened)) {
-      //banner為open狀態，執行關閉
       this.close();
     } else if (this.$ele.hasClass(this.closed)) {
-      //banner為close狀態, 執行開啟
       this.open();
     }
+    clearInterval(this.timer_num); //執行一次點擊就取消toggle
+    clearInterval(this.timer_bln);
     this.Transition();
-    clearInterval(this.timer); //因為toggle包含click
   };
   Module.prototype.open = function() {
     //需要精簡一下
     this.$wrap.removeClass("closed").addClass("opened");
     this.$btn.text(this.option.button.closeText).append(this.$up);
-    this.$img.removeClass("closed").addClass("opened");
-    if (this.option.transition === true) {
+    this.$img.removeClass("closed").addClass("opened");  // open: 圖先出來
+    if (this.option.transition === true) {     
       this.$ele.removeClass(this.closed).addClass(this.option.class.opening);
       setTimeout(
         function() {
@@ -114,36 +130,37 @@
       );
     } else {
       // this.$img.removeClass("closed").addClass("opened");
-      this.$ele.removeClass(this.closed).addClass(this.option.class.opening);
+      this.$ele.removeClass(this.closed).addClass(this.option.class.opened);
     }
+    //this.Transition();
   };
   Module.prototype.close = function() {
     //需要精簡一下
     this.$wrap.removeClass("opened").addClass("closed");
     this.$btn.text(this.option.button.openText);
     this.$btn.append(this.$down);
-    let imgAction = setTimeout(
+    
+    if (this.option.transition === true) {
+      this.$ele.removeClass(this.opened).addClass(this.option.class.closing);
+      setTimeout(
       function() {
-        this.$img.removeClass("opened").addClass("closed");
+        this.$img.removeClass("opened").addClass("closed");   //close: 圖最後才出來
       }.bind(this),
       this.option.counter
     );
-
-    if (this.option.transition === true) {
-      this.$ele.removeClass(this.opened).addClass(this.option.class.closing);
-      imgAction;
-      setTimeout(
+      setTimeout(  //在transtion結束的時候卡入closing
         function() {
           this.$ele
             .removeClass(this.option.class.closing)
             .addClass(this.closed);
         }.bind(this),
-        this.option.counter
+        this.option.counter 
       );
     } else {
+      this.$img.removeClass("opened").addClass("closed");   //因為沒有transition需要立刻處理
       this.$ele.removeClass(this.opened).addClass(this.closed);
-      imgAction;
     }
+    //this.Transition();  
   };
 
   Module.prototype.Transition = function() {
@@ -157,10 +174,12 @@
           this.option.whenTransition();
         }
       }.bind(this),
-        500
+        25
     );
   };
-  $.fn[ModuleName] = function(options, options2) {
+  
+  
+  $.fn[ModuleName] = function(options) {
     return this.each(function() {
       var $this = $(this);
       var module = $this.data(ModuleName);
@@ -179,15 +198,16 @@
           typeof options === "object" && options,
           typeof options2 === "Obect" && options2
         );
+        module = new Module(this, opts);
+        $(this).data(ModuleName, module);
+        //初始化
+        module.init();
+        //註冊btn事件
+        module.$btn.on("click", function(e) {
+          module.click();
+        });
       }
-      module = new Module(this, opts);
-      $(this).data(ModuleName, module);
-      //初始化
-      module.init();
-      //註冊btn事件
-      module.$btn.on("click", function(e) {
-        module.click();
-      });
+     
     });
   };
 })(jQuery);
